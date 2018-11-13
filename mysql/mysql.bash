12b66ae531e4105e1032368bbd9ca873c79af5be
zhanchenjin <zhanchenjin@magefitness.com>
2018-11-13 10:21:59 +0800

    * 修改mysql脚本

--- mysql/mysql.bash
+++ mysql/mysql.bash
@@ -1,9 +1,55 @@
-docker run -d  -p 5000:5000 --restart=always --name registry -v /srv/registry/data:/var/lib/registry registry:2	
+#!/bin/bash
 
-sudo docker create --name=mysqlcopy mysql/mysql-server:5.7.24 --character-set-server=utf8mb4  --collation-server=utf8mb4_col
+if [ $# -lt 1 ] ; then
+    cmd=run_with_volume
+else
+    cmd=$1
+fi
 
-sudo docker cp mysqlcopy:/etc/my.conf /srv/mysql/config/my.conf
 
-sudo docker rm mysqlcopy
+run_without_volume() { # 没有volume的启动方式
+	
+	docker run  --name=mysql -d -p 3306:3306  --restart always  -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_ROOT_HOST=% mysql/mysql-server:5.7.24
+}
 
-sudo docker run --name=mysql -d --restart always  -e MYSQL_ROOT_PASSWORD=123456 -v /srv/mysql/config/my.conf:/etc/my.cnf  -v /srv/mysql/data:/var/lib/mysql mysql/mysql-server:5.7.24
+
+run_with_volume() { # volume的启动方式
+	
+	docker create --name=mysqlcopy mysql/mysql-server:5.7.24
+		
+	rm -rf /srv/mysql /srv/mysql
+
+	mkdir -p /srv/mysql/config /srv/mysql/data
+
+	docker cp mysqlcopy:/etc/my.cnf /srv/mysql/config/
+
+	docker rm mysqlcopy
+	
+	# MYSQL ROOT HOST的设置需要在/var/lib/mysql没有创建之前
+	docker run --name=mysql -d -p 3306:3306  --restart always  \
+		-e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_ROOT_HOST=% \
+		-v /srv/mysql/config/my.cnf:/etc/my.cnf -v /srv/mysql/data:/var/lib/mysql \
+		mysql/mysql-server:5.7.24 
+}
+
+
+rm() { # 删除容器
+
+	docker stop mysql
+	docker rm mysql
+}
+		
+		
+		
+case $cmd in
+run_with_volume)
+    run_with_volume
+;;
+run_without_volume)
+    run_without_volume
+;;
+rm)
+    rm
+;;
+esac
+exit 0
--- registry/registry.bash
+++ registry/registry.bash
@@ -1,4 +1,6 @@
-docker run -d  -p 5000:5000 --restart=always --name registry -v /srv/registry/data:/var/lib/registry registry:2	
+#!/bin/bash
+
+docker run -d  -p 5000:5000 --restart=always --name registry -v /srv/registry/data:/var/lib/registry registry:2
 
 # 搭建的http服务不是docker的授信服务，docker仅支持https，所以需要修改docker配置文件
 # 在”/etc/docker/“目录下，创建”daemon.json“文件。在文件中写入：{ "insecure-registries":["192.168.1.xxx:5000"] }	
